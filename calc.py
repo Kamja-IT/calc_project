c8781a0f9a4eee7a4f17451adfd54880e5f67cf5
Kamja-IT <hichi9897@gmail.com>
2026-08-04 11:25:36 +0900

    ignore 추가 : text.txt

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@ def sum(a, c):
     return a + c
 
 def sub(a, b):
+<<<<<<< HEAD
     return a - b
 
 def mul(a, b):
@@ -9,3 +10,6 @@ def mul(a, b):
 
 def div(a, b):
     return a / b
+=======
+    return a - b
+>>>>>>> parent of e5d6a08 (곱하기 함수 추가)
